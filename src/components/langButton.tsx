cd529902aa1cf53a05f740f5597f64bcee86e664
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-07-06 05:33:15 +0300

    adding millifiy to the loc

--- src/components/langButton.tsx
+++ src/components/langButton.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { isTouchDevice } from "../utils/utils"
+import {millify} from 'millify'
 
 export function LangButton({data,idx}:{data:TTop,idx:number}){
     const [open,setOpen] = useState<boolean>(false)
@@ -19,7 +20,7 @@ export function LangButton({data,idx}:{data:TTop,idx:number}){
             <button onClick={handleClick} className="overflow-hidden backdrop-blur transition-max-height duration-500  bg-gray-200/50 hover:bg-white/100 w-full rounded-lg py-1 px-4  bg-white">
             <div className="flex justify-between">
                 <div>#{idx} {data.lang.name}</div>
-                <div>{data.lang.fileSize}</div>
+                <div>{millify(data.lang.fileSize)}</div>
             </div>
             <div className={`px-6 pt-0 overflow-hidden transition-[max-height] duration-500 ease-in-out ${open ? "max-h-40" : "max-h-0"}`}>
                 <ul className="flex flex-col justify-start text-start">
@@ -42,7 +43,7 @@ export function LangButton({data,idx}:{data:TTop,idx:number}){
         <button onMouseEnter={()=> setOpen(true)} onMouseLeave={()=> setOpen(false)} className="overflow-hidden backdrop-blur transition-max-height duration-500  bg-gray-200/50 hover:bg-white/100 w-full rounded-lg py-1 px-4  bg-white">
             <div className="flex justify-between">
                 <div>#{idx} {data.lang.name}</div>
-                <div>{data.lang.fileSize}</div>
+                <div>{millify(data.lang.fileSize)}</div>
             </div>
             <div className={`px-2 pt-0 overflow-hidden transition-[max-height] duration-500 ease-in-out ${open ? "max-h-40" : "max-h-0"}`}>
                 <ul className="flex flex-col justify-start text-start">
